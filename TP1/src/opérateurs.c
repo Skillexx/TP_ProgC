b4954c091458b5de9d71bbe9c2a70b4db0c95e32
Skillexx <138004219+Skillexx@users.noreply.github.com>
2025-10-24 11:59:49 +0200

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+int a = 16
+int b = 3
+
+int somme(a, b) {
+  somme = a + b;
+return somme;
+
+int soustraction(a, b) {
+  soustraction = b - a;
+return soustraction;
+
+int multiplication(a, b) {
+  multiplication = a * b;
+return multiplication;
+
+int division(a, b) {
+  multiplication = a / b;
+return division;
+
+int modulo(a, b) {
+  modulo = a%b;
+return modulo;
