164fe77263504d49fe9f4779f40df851b029a3bd
Skillexx <138004219+Skillexx@users.noreply.github.com>
2025-12-02 10:52:57 +0100

    Update boucles.c
    Normalement c'est mieux, mais a revoir

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(){
+int main() {
   int compteur = 5; // On inité la vaible compteur a 5
 
 if(compteur > 9){
@@ -8,25 +8,27 @@ if(compteur > 9){
   return 1; // Permet de mettre un code erreur pour savoir d'où provient l'erreur
 }
 
-for (int i=1, i<compteur, i++){
-  for(int j = 1, j < i, j++){
+for (int i=1; i<compteur; i++){
+  for(int j = 1; j < i; j++){
     if (i == 1) {
       printf("* "); // Pour la premiere ligne, ça commence forcément par une *
-
+    }
+    if(i==2){
+      printf("** "); //Pareil pour la deuxieme
+    }
+    
     else if (i==compteur){
       printf("* "); // derniere ligne en étoiles
     }
-
-    }
     else {
-        // lignes intermédiaires
-        if (j % 2 == 1)
-            printf("* ");   // positions impaires → *
-        else
-            printf("# ");   // positions paires → #
+        if (j == 1)
+          printf("*"); 
+        else {
+          printf("#");
         }
     }
-    printf("\n");
+    printf("\n"); // Pour retourner a la ligne apres chaques passage en dessous, a voir si il est au bon endroit
   }
   return 0;
 }
+
