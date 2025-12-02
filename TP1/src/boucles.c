8c992500e4af3889d4d1430247fbdfb9dcb4089f
Skillexx <138004219+Skillexx@users.noreply.github.com>
2025-12-02 11:03:16 +0100

    Update boucles.c
    retest...

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -8,12 +8,12 @@ if(compteur > 9){
   return 1; // Permet de mettre un code erreur pour savoir d'où provient l'erreur
 }
 
-for (int i=1; i<compteur; i++){
-  for(int j = 1; j < i; j++){
+for (int i=1; i<=compteur; i++){
+  for(int j = 1; j <= i; j++){
     if (i == 1) {
       printf("* "); // Pour la premiere ligne, ça commence forcément par une *
     }
-    if(i==2){
+    else if(i==2){
       printf("** "); //Pareil pour la deuxieme
     }
     
@@ -27,8 +27,9 @@ for (int i=1; i<compteur; i++){
           printf("#");
         }
     }
-    printf("\n"); // Pour retourner a la ligne apres chaques passage en dessous, a voir si il est au bon endroit
   }
+  printf("\n"); // Pour retourner a la ligne apres chaques passage en dessous, a voir si il est au bon endroit
   return 0;
 }
+
 
